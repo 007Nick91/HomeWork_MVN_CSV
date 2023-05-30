8929d4ea933fcff6d152a782bff8da09ba990f5f
Niko test <Lesnik6.6.6@mail.ru>
2023-05-30 22:20:45 +0300

    Version two

--- src/test/java/ru/netology/HomeWork_MVN_CSV/Servises/yearcalculateTest.java
+++ src/test/java/ru/netology/HomeWork_MVN_CSV/Servises/yearcalculateTest.java
@@ -2,29 +2,42 @@ package ru.netology.HomeWork_MVN_CSV.Servises;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 //import ru.netology.HomeWork_MVN_CSV.Servises.yearcalcullate;
 
 public class yearcalculateTest {
 
-    @Test
-    public void shouldCalculatefirst() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/count.csv")
+    public void shouldCalculatefirst(int income, int expenses, int threshold) {
         yearcalcullate service = new yearcalcullate();
-        int income = 10000;
-        int expenses = 3000;
-        int threshold = 20000;
+
         int year = service.calculate(income, expenses, threshold);
 
         System.out.println(year);
     }
 
-    @Test
-    public void shouldCalculatesecond() {
-        yearcalcullate service = new yearcalcullate();
-        int income = 100000;
-        int expenses = 60000;
-        int threshold = 150000;
-        int year = service.calculate(income, expenses, threshold);
+    // @Test
+    //  public void shouldCalculatefirst() {
+    //  yearcalcullate service = new yearcalcullate();
+    // int income = 10000;
+    // int expenses = 3000;
+    // int threshold = 20000;
+    // int year = service.calculate(income, expenses, threshold);
 
-        System.out.println(year);
-    }
+    //      System.out.println(year);
+    // }
+
+    //@Test
+    //public void shouldCalculatesecond() {
+    //  yearcalcullate service = new yearcalcullate();
+    //int income = 100000;
+    //int expenses = 60000;
+    // int threshold = 150000;
+    //int year = service.calculate(income, expenses, threshold);
+
+    // System.out.println(year);
+    //  }
 }
