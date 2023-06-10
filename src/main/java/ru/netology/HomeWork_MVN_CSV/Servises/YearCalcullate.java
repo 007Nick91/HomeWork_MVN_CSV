5febd91e079f7b1804f21b422f5be8c1f4e04ab3
Niko test <Lesnik6.6.6@mail.ru>
2023-06-10 12:38:00 +0300

    Changa again

--- src/main/java/ru/netology/HomeWork_MVN_CSV/Servises/YearCalcullate.java
+++ src/main/java/ru/netology/HomeWork_MVN_CSV/Servises/YearCalcullate.java
@@ -2,13 +2,14 @@ package ru.netology.HomeWork_MVN_CSV.Servises;
 
 public class YearCalcullate {
     public int calculate(int income, int expenses, int threshold) {
-        int rest = expenses * 3;
+
         int count = 0;
         int money = 0;
         for (int month = 0; month < 12; month++) {
-            if (money > threshold) {
+            if (money >= threshold) {
+                money -= expenses;
+                money /=3;
                 count++;
-                money = money - rest - expenses;
             } else {
                 money = income - expenses + money;
             }
